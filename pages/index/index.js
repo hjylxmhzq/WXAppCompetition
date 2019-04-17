618b8f92fc11522c420edc2ee2188ffa18473bf1
Ricardo <ricardolcao@gmail.com>
2019-04-17 23:11:06 +0800

    pages/index style

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    cardsArray: [],
-    todayClassesArray: []
+    remindArray: [{
+      title: "111",
+      time: "2019.5.1",
+      timeLeft: "30min",
+      text: "1111111111111111"
+    }, {
+      title: "222",
+      time: "2019.5.2",
+      timeLeft: "30min",
+      text: "2222222222222222"
+    }],
+    todayClassesArray: [{
+      title: "课程1",
+      time: "3、4节",
+      place: "1104",
+      text: "???"
+    }, {
+      title: "课程2",
+      time: "7、8节",
+      place: "4203",
+      text: "???"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.request({
       url: "tony-space.top",
       data: {
@@ -23,7 +43,7 @@ Page({
       success: res => {
         if (res.statusCode === 200) {
           this.setData({
-            
+
           });
         }
       }
@@ -33,7 +53,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
       title: "某课程表",
       path: "/pages/index/index?fromUserId=" + wx.getStorageSync('session_id'),
