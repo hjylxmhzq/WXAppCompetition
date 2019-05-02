079ce12765f54b4b8468d6cc94ebb0ba842db324
Ricardo <ricardolcao@gmail.com>
2019-05-02 12:47:07 +0800

    pages/my

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,62 +5,71 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    settings: [{
+      icon: "/icon/set.png",
+      text: "设置"
+    }, {
+      icon: "/icon/share.png",
+      text: "分享"
+    }, {
+      icon: "/icon/about.png",
+      text: "关于我们"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
       title: "某课程表",
       path: "/pages/index/index?fromUserId=" + wx.getStorageSync('session_id'),
