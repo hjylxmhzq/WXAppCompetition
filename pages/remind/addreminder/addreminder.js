e404c3721b36d2804c6460f5b0d22e3a37d1704e
hjylxmhzq <hujingyuan25@163.com>
2019-05-06 00:52:55 +0800

    增加remind/addremind界面

--- pages/remind/addreminder/addreminder.js
+++ pages/remind/addreminder/addreminder.js
@@ -16,78 +16,110 @@ Page({
     inwayIndex: 0,
     inwayArray: ['微信']
   },
-  
+
 
   bindPickerChange(e) {
     this.setData({
       courseIndex: e.detail.value
     })
   },
-  bindDateChange: function (e) {
+  bindDateChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       date: e.detail.value
     })
   },
-  bindTimeChange: function (e) {
+  bindTimeChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       time: e.detail.value
     })
   },
+  submitform(e) {
+    let data = {
+      content: e.detail.value['reminder-content'],
+      mark: e.detail.value['reminder-mark'],
+      course: this.data.courseArray[this.data.courseIndex],
+      date: this.data.date,
+      time: this.data.time,
+      inway: this.data.courseArray[this.data.courseIndex]
+    }
+
+    wx.request({
+      url: 'http://127.0.0.1:8005/addreminder',
+      data,
+      method: 'POST',
+      header: {
+        'content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': "app:sess="+wx.getStorageSync("session_id")
+      },
+      success: function (res, statusCode) {
+        if (res.statusCode == 201) {
+          wx.navigateTo({
+            url: '/pages/remind/remind',
+          })
+        } else {
+          console.log('add reminder fail');
+        }
+      },
+      fail: function(res) {
+        console.log('fail to add reminder');
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/remind/remind.js
+++ pages/remind/remind.js
@@ -1,32 +1,48 @@
 // pages/remind/remind.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    cardDate: '4月16日',
+    cardDay: '星期四',
+    cardCourse: '高等数学',
+    cardTime: '8:00',
+    cardContent: '第四章随堂小测',
+    cardMark: '注：本章内容较多'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this;
+    wx.request({
+      url: 'http://127.0.0.1:8005/getreminder',
+      dataType: 'json',
+      method: 'POST',
+      header: {
+        'Cookie': "app:sess=" + wx.getStorageSync("session_id")
+      },
+      success: function(res) {
+        that.setData({remindData: res.data});
+      },
+      fail: function() {
+        console.log('request reminder data fail');
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
